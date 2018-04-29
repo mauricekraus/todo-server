b055656c3293aaa31890b97bd185b3cd9e642274
Maurice Kraus <maurice.kraus@stud.tu-darmstadt.de>
2018-04-29 16:12:01 +0200

    created a hidden uri and updated the gitignore

--- server.js
+++ server.js
@@ -2,16 +2,25 @@
 
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const nconf = require('nconf');
 const bodyParser = require('body-parser');
 let db = require('./config/db');
 
 const app = express();
-
 const port = 8000;
+
+nconf.argv().env().file('keys.json');
+const user = nconf.get('mongoUser');
+const pass = nconf.get('mongoPass');
+const host = nconf.get('mongoHost');
+const port = nconf.get('mongoPort');
+const database = nconf.get('mongoDatabase');
+
+let uri = `mongodb://${user}:${pass}@${host}:${port}/${database}`;
 
 app.use(bodyParser.urlencoded({ extended: true}));
 
-MongoClient.connect(db.url, (err, database) => {
+MongoClient.connect(uri, (err, database) => {
     if (err) {
         return console.log(err);
     } 
