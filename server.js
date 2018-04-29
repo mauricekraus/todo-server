191767e9d03f1f04df7d85e39ddf19d4456bc3af
Maurice Kraus <maurice.kraus@stud.tu-darmstadt.de>
2018-04-29 16:16:40 +0200

    fixed bug preventing the server to start

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const nconf = require('nconf');
 const bodyParser = require('body-parser');
-let db = require('./config/db');
 
 const app = express();
 const port = 8080;
@@ -13,18 +12,18 @@ nconf.argv().env().file('keys.json');
 const user = nconf.get('mongoUser');
 const pass = nconf.get('mongoPass');
 const host = nconf.get('mongoHost');
-const port = nconf.get('mongoPort');
+const mongoPort = nconf.get('mongoPort');
 const database = nconf.get('mongoDatabase');
 
-let uri = `mongodb://${user}:${pass}@${host}:${port}/${database}`;
+let uri = `mongodb://${user}:${pass}@${host}:${mongoPort}/${database}`;
+console.log(uri);
 
 app.use(bodyParser.urlencoded({ extended: true}));
-
 MongoClient.connect(uri, (err, database) => {
     if (err) {
         return console.log(err);
     } 
-    db = database.db("todo-db");
+    const db = database.db("todo-db");
     require('./app/routes')(app,db);
     app.listen(port, () => {
         console.log('We are live on ' + port);
